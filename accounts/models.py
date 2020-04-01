e20086089feef14f1640b83cd0fba846e59f32e9
prashantchanne12 <prashantchanne121@gmail.com>
2020-04-01 20:17:27 +0530

    Added tabels and linked pages

--- accounts/models.py
+++ accounts/models.py
@@ -12,6 +12,13 @@ class Customer(models.Model):
 	def __str__(self):
 		return self.name
 
+class Tag(models.Model):
+	name = models.CharField(max_length=200, null=True)
+
+	def __str__(self):
+		return self.name
+
+
 class Product(models.Model):
 
 	CATEGORY = ( ('Indoor','Indoor'),
@@ -23,6 +30,13 @@ class Product(models.Model):
 	category =  models.CharField(max_length=200, null=True, choices=CATEGORY)
 	description =  models.CharField(max_length=200, null=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	tag = models.ManyToManyField(Tag)
+
+
+	def __str__(self):
+		return self.name
+
+
 
 
 class Order(models.Model):
@@ -31,7 +45,10 @@ class Order(models.Model):
 			  ('Out for delivery', 'Out for delivery'),
 			  ('Delivered','Delivered')
 			 )
-	# customer = 
-	# product =
+	customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+	product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+	def __str__(self):
+		return f'{self.customer} - {self.product}'
--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('customers/', views.customers),
-    path('products/', views.products),
+    path('', views.home, name='home'),
+    path('customers/<str:pk>/', views.customers, name='customers'),
+    path('products/', views.products, name='products'),
 ]
